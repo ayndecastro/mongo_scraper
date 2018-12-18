ea9d72a7f758e8df2c00c4c3641f61a3462b096e
ayn <vincentayndecastro@gmail.com>
2018-12-18 13:07:05 -0600

    final

--- routes/routes.js
+++ routes/routes.js
@@ -13,7 +13,7 @@ module.exports = function(app){
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with axios
-    axios.get("http://www.espn.com/nba/").then(function(response) {
+    axios.get("https://www.espn.com/nba/").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
   
